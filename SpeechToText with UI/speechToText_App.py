d56f4ad96fd8b7e40635f80354d483b4634cb86f
Kaan Dincer <54600467+kaandincer@users.noreply.github.com>
2023-12-01 12:19:31 -0500

    File updated.

--- SpeechToText with UI/speechToText_App.py
+++ SpeechToText with UI/speechToText_App.py
@@ -1,26 +1,46 @@
-import streamlit as st
-# house all the code for web app. Provides with input/output widgets.
-import websockets
-# interact with Assembly AI
-import asyncio
-# perform audio input and output in concurrent manner
-import base64
-# encode and decode audio signals before sent to api
-import json
-# read in audio output (transcribe text)
-import pyaudio
-# accept audio input processing 
-import os
+# import sys
+# sys.path.append("/Users/kaandincer/Library/Python/3.9/lib/python/site-packages")
+import streamlit as st # house all the code for web app. Provides with input/output widgets.
+# from docx import Document
+import websockets # interact with Assembly AI
+import asyncio # perform audio input and output in concurrent manner
+import base64 # encode and decode audio signals before sent to api
+import json # read in audio output (transcribe text)
+import pyaudio # accept audio input processing
+import os # for navigating through the various files in the project and file processing
 from pathlib import Path
-# for navigating through the various files in the project and file processing
 
-# Session state
+# Describing session state (memory of the web app)
 if 'text' not in st.session_state:
-	st.session_state['text'] = 'Listening...'
+	st.session_state['text'] = 'Listening...' # if running for the first time, session state text equals listening
 	st.session_state['run'] = False
+	st.session_state['recording_name'] = ''
+	st.session_state['recorded_files'] = []  # New key for recorded files
 
-# Audio parameters 
+# Audio parameters. We create a sidebar to set audio parameters 
 st.sidebar.header('Audio Parameters')
+
+# Web user interface
+st.title('Auto-Document')
+st.header('🎙️ Record Your Meeting Notes')
+
+with st.expander('About this App'):
+	st.markdown('''
+	This web app uses the AssemblyAI API to perform real-time speech to text transcription.
+	
+	Things to note:
+	- If you want to change the look of the web app or other settings, click the 3 bars on the upper right corner of your screen, and click settings. 
+	- If you want to change audio parameters of your recording, click the arrow on the upper left corner, and set the numbers. 
+	- We reccomend keeping the parameters at their pre-set levels. 
+	- Click the "Download Meeting Notes" to download the transcription (of your meeting) to your local device.
+	''')
+
+recording_name = st.text_input('Enter a name for the recording:')
+st.session_state['recording_name'] = recording_name
+
+# # Display the chosen recording name
+# if st.session_state['recording_name']:
+#     st.write(f"Recording Name: {st.session_state['recording_name']}")
 
 FRAMES_PER_BUFFER = int(st.sidebar.text_input('Frames per buffer', 3200))
 FORMAT = pyaudio.paInt16
@@ -42,36 +62,36 @@ def start_listening():
 	st.session_state['run'] = True
 
 def download_transcription():
+	file_name = f"{st.session_state['recording_name'] or 'meeting_recording'}.txt"
 	read_txt = open('transcription.txt', 'r')
 	st.download_button(
-		label="Download transcription",
+		label="Download Meeting Notes",
 		data=read_txt,
-		file_name='transcription_output.txt',
+		# file_name='meeting_recording.txt',
+		file_name = file_name,
 		mime='text/plain')
+
+# .docx download
+# def download_transcription():
+#     document = Document()
+#     document.add_paragraph(st.session_state['text'])
+
+#     document.save('transcription_output.docx')
+
+#     st.download_button(
+#         label="Download Meeting Notes",
+#         data='transcription_output.docx',
+#         file_name='transcription_output.docx',
+#         mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+#     )
 
 def stop_listening():
 	st.session_state['run'] = False
 
-# Web user interface
-st.title('🎙️ Real-Time Transcription App')
+start, stop = st.columns(2)
 
-with st.expander('About this App'):
-	st.markdown('''
-	This Streamlit app uses the AssemblyAI API to perform real-time transcription.
-	
-	Libraries used:
-	- `streamlit` - web framework
-	- `pyaudio` - a Python library providing bindings to [PortAudio](http://www.portaudio.com/) (cross-platform audio processing library)
-	- `websockets` - allows interaction with the API
-	- `asyncio` - allows concurrent input/output processing
-	- `base64` - encode/decode audio data
-	- `json` - allows reading of AssemblyAI audio output in JSON format
-	''')
-
-col1, col2 = st.columns(2)
-
-col1.button('Start', on_click=start_listening)
-col2.button('Stop', on_click=stop_listening)
+start.button('Start', on_click=start_listening)
+stop.button('Stop', on_click=stop_listening)
 
 # Send audio (Input) / Receive transcription (Output)
 async def send_receive():
@@ -145,11 +165,14 @@ async def send_receive():
 
 asyncio.run(send_receive())
 
+# After clicking on stop, download button shows, from this code. 
 if Path('transcription.txt').is_file():
 	st.markdown('### Download')
+	st.sidebar.markdown('### Recorded Files')
 	download_transcription()
 	os.remove('transcription.txt')
 
 # References (Code modified and adapted from the following)
 # 1. https://github.com/misraturp/Real-time-transcription-from-microphone
 # 2. https://medium.com/towards-data-science/real-time-speech-recognition-python-assemblyai-13d35eeed226
+# 3. https://www.youtube.com/watch?v=8rb9GefC_CU
